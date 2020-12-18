c26ad334e6047012021e0418a57b7898c9f353a2
César Carruitero <ccarruitero@protonmail.com>
2020-12-18 16:48:30 -0500

    add test for sheet controller customization

--- test/components/spreadsheet/sheet_test.rb
+++ test/components/spreadsheet/sheet_test.rb
@@ -6,4 +6,12 @@ class SheetTest < ViewComponent::TestCase
 
     assert_selector("div.spreadsheet--sheet", text: "Ahoy!")
   end
+
+  def test_customize_component_controller
+    render_inline(::Spreadsheet::Sheet.new())
+    assert_selector("div.spreadsheet--sheet[data-controller='spreadsheet--sheet']")
+
+    render_inline(::Spreadsheet::Sheet.new(controller: 'mycustom-controller'))
+    assert_selector("div.spreadsheet--sheet[data-controller='mycustom-controller']")
+  end
 end
