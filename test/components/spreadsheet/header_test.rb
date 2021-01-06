dc2f343573df119095b0fb0765d5d423df3c885f
César Carruitero <ccarruitero@protonmail.com>
2021-01-06 18:10:45 -0500

    adding some tests for header

--- test/components/spreadsheet/header_test.rb
+++ test/components/spreadsheet/header_test.rb
@@ -9,4 +9,24 @@ class HeaderTest < ViewComponent::TestCase
 
     assert_selector('div.spreadsheet--header')
   end
+
+  def test_dropdown
+    render_inline(::Spreadsheet::Header.new(
+      id: 'header-test',
+      columns: %i[column0 column1 column2]
+    )) do |component|
+      component.with(:context_menu, 'Header Menu')
+    end
+
+    assert_selector('sl-dropdown', text: 'Header Menu')
+  end
+
+  def test_search
+    render_inline(::Spreadsheet::Header.new(
+      id: 'header-test',
+      columns: %i[column0 column1 column2]
+    ))
+
+    assert_selector('div.spreadsheet--search')
+  end
 end
