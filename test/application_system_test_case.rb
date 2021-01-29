0ea77c627ceb6d55d515a9ee0b1696288e497226
César Carruitero <ccarruitero@protonmail.com>
2021-01-28 19:14:59 -0500

    show js errors for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_firefox
+  driven_by :selenium, using: :headless_chrome
+
+  teardown do
+    errors = page.driver.browser.manage.logs.get(:browser)
+    if errors.present?
+      errors.each do |error|
+        assert error.level != 'SEVERE', error.message
+        next unless error.level == 'WARNING'
+
+        warn "\e[33mJavaScript warning:"
+        warn error.message
+      end
+    end
+  end
 end
